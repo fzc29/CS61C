8eb12a86359a48eda7c8efdcf11754ffd2c89bff
Jade Chen <104612549+fzc29@users.noreply.github.com>
2025-08-14 17:43:22 -0700

    Add files via upload

---
+++ 61c-proj3/tests/integration-custom/in/i-type.s
@@ -0,0 +1,19 @@
+addi t0 t0 8
+
+andi t0 t0 0
+
+addi t0 t0 8
+
+ori t0 t0 0x00
+
+xori t0 t0 0xFF
+
+srli t0 t0 4
+
+slli t0 t0 4
+
+addi x5, -16
+srai x6, x5, 2
+
+addi x5, 5
+slti x6, x5, 10
---
+++ 61c-proj3/tests/integration-custom/in/r-type.s
@@ -0,0 +1,18 @@
+addi t0 x0 1
+addi t1 x0 3
+addi t2 x0 8
+
+add t3 t0 t1
+
+sub t4 t2 t3
+
+and t5 t3 t4
+and t5 t5 t1
+
+or t6 t0 t1
+
+xor t3 t0 t2
+
+sll t4 t2 t0
+
+srl t5 t2 t0
